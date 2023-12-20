1bb242c6454bf3bbfea4e3cf4b33be22e58c47cc
DESKTOP-3T85H9P\Adrian <csi23-awanuer@colegioaltair.net>
2023-12-20 10:15:55 +0100

    Agregar archivos de proyecto.

---
+++ Actividad8Arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Actividad8Arrays
+{
+    class program
+    {
+        public static void Main(String[] args) 
+        {
+            string[] palabras = { "Hola", "Adiós", "Nuevo", "Viejo", "Vamos" };
+
+            int busqueda = Array.IndexOf(palabras, "Adiós");
+            if (busqueda == -1 )
+            {
+                Console.WriteLine("No esta en la lista");
+            }
+            else
+            {
+                Console.WriteLine("La palabra adios existe");
+            }
+
+            int[] numeros = { 1, 5, 7, 8, 4, 3, 9 };
+
+
+            var numerosMayores = numeros.Where(numeros => numeros > 3);
+            foreach ( var numero in numerosMayores)
+            {
+                Console.WriteLine(numero) ;
+            }
+            
+        }
+    }
+}
